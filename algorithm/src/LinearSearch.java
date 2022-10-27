6cb32b22b460c575456ec0cfa4bdb5630c8a40df
NYK <kny0339@gmail.com>
2022-10-27 18:19:14 +0900

    2022-10-27 선형검색

--- algorithm/src/LinearSearch.java
+++ algorithm/src/LinearSearch.java
@@ -1,16 +1,25 @@
+import java.util.*;
 
 public class LinearSearch {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		linearSearch(0, null);
+		int num = sc.nextInt();
+		int[] arr = new int[num+1];
+		for(int i=0;i<num;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int key = sc.nextInt();
+		int result = linearSearch(key, arr);
+		if(result == -1) System.out.println("값없음");
+		else System.out.println((result+1) + "번째에 있음");
 	}
 	public static int linearSearch(int key,int[] arr) {
-		int i = 0;
 		int n = arr.length;
-		while(true) {
-			if(i == n) return -1;
+		for(int i=0;i<n;i++) {
 			if(arr[i] == key) return i;
-			i++;
 		}
+		return -1;
 	}
 }
